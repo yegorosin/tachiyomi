559f61b68d4363a0825746c2b98ac8c8dad3fe98
Yegor Osin <yegorosin@gmail.com>
2022-05-23 00:28:41 +0700

    [UI-3] Add methods to BrowseRobot

--- app/src/androidTest/java/eu/kanade/tachiyomi/robot/screens/browse/BrowseRobot.kt
+++ app/src/androidTest/java/eu/kanade/tachiyomi/robot/screens/browse/BrowseRobot.kt
@@ -1,3 +1,15 @@
 package eu.kanade.tachiyomi.robot.screens.browse
 
-class BrowseRobot
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.utils.Waiting.waitForView
+
+class BrowseRobot {
+
+    fun waitForScreen() = waitForView(R.id.pager)
+
+    fun assertBrowseTabWasShown() {
+        assertDisplayed(R.id.tabs)
+    }
+
+}
